e7f1541f8a054382f66ebc03fc22d8f3a527b190
Yitzi Ginzberg <emesdg@gmail.com>
2020-05-07 17:11:22 +0300

    remove unused field

--- src/main/java/video/api/java/sdk/domain/video/VideoInput.java
+++ src/main/java/video/api/java/sdk/domain/video/VideoInput.java
@@ -6,12 +6,12 @@ import java.util.List;
 import java.util.Map;
 
 public class VideoInput {
-    public       String              description;
-    public       boolean             isPublic = true;
+
     public final Map<String, String> metadata = new HashMap<>();
-    public       boolean             panoramic;
-    public       String              playerId;
-    public       String              source;
-    public final List<String>        tags     = new ArrayList<>();
-    public       String              title;
+    public final List<String> tags = new ArrayList<>();
+    public String description;
+    public boolean isPublic = true;
+    public boolean panoramic;
+    public String playerId;
+    public String title;
 }
