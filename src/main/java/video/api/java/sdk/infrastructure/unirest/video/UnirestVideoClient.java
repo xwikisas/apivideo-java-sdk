a0057bbb764a5c6ffb91fdc9e68be6dbb41c8186
Yitzi Ginzberg <emesdg@gmail.com>
2020-05-07 17:01:34 +0300

    optimize imports

--- src/main/java/video/api/java/sdk/infrastructure/unirest/video/UnirestVideoClient.java
+++ src/main/java/video/api/java/sdk/infrastructure/unirest/video/UnirestVideoClient.java
@@ -5,11 +5,7 @@ import org.json.JSONObject;
 import video.api.java.sdk.domain.QueryParams;
 import video.api.java.sdk.domain.exception.ResponseException;
 import video.api.java.sdk.domain.pagination.PageQuery;
-import video.api.java.sdk.domain.video.Status;
-import video.api.java.sdk.domain.video.UploadProgressListener;
-import video.api.java.sdk.domain.video.Video;
-import video.api.java.sdk.domain.video.VideoClient;
-import video.api.java.sdk.domain.video.VideoInput;
+import video.api.java.sdk.domain.video.*;
 import video.api.java.sdk.infrastructure.pagination.IteratorIterable;
 import video.api.java.sdk.infrastructure.pagination.PageIterator;
 import video.api.java.sdk.infrastructure.unirest.RequestExecutor;
@@ -25,19 +21,20 @@ import static java.lang.Math.min;
 import static kong.unirest.HttpMethod.*;
 
 public class UnirestVideoClient implements VideoClient {
+
     private static final int CHUNK_SIZE = 128 * 1024 * 1024; // 128 MB
 
-    private final RequestBuilderFactory      requestBuilderFactory;
+    private final RequestBuilderFactory requestBuilderFactory;
     private final JsonSerializer<VideoInput> serializer;
-    private final JsonDeserializer<Video>    deserializer;
-    private final RequestExecutor    requestExecutor;
+    private final JsonDeserializer<Video> deserializer;
+    private final RequestExecutor requestExecutor;
     private final StatusDeserializer statusDeserializer = new StatusDeserializer();
 
     public UnirestVideoClient(RequestBuilderFactory requestBuilderFactory, JsonSerializer<VideoInput> serializer, JsonDeserializer<Video> deserializer, RequestExecutor requestExecutor) {
         this.requestBuilderFactory = requestBuilderFactory;
-        this.serializer            = serializer;
-        this.deserializer          = deserializer;
-        this.requestExecutor       = requestExecutor;
+        this.serializer = serializer;
+        this.deserializer = deserializer;
+        this.requestExecutor = requestExecutor;
     }
 
     public Video get(String videoId) throws ResponseException {
@@ -95,7 +92,8 @@ public class UnirestVideoClient implements VideoClient {
         String videoId;
         if (videoInput instanceof Video) {
             videoId = ((Video) videoInput).videoId;
-        } else {
+        }
+        else {
             if (videoInput.title == null) {
                 videoInput.title = file.getName();
             }
@@ -106,7 +104,8 @@ public class UnirestVideoClient implements VideoClient {
 
         try {
             Thread.sleep(150);
-        } catch (InterruptedException e) {
+        }
+        catch (InterruptedException e) {
             throw new IllegalArgumentException(e);
         }
 
@@ -119,64 +118,16 @@ public class UnirestVideoClient implements VideoClient {
             JsonNode responseBody;
             if (fileLength < CHUNK_SIZE) {
                 responseBody = uploadSingleRequest(listener, file, videoId);
-            } else {
+            }
+            else {
                 responseBody = uploadMultipleRequests(file, listener, videoId, fileLength);
             }
 
             return deserializer.deserialize(responseBody.getObject());
-        } catch (IOException e) {
+        }
+        catch (IOException e) {
             throw new IllegalArgumentException(e.getMessage());
         }
-
-    }
-
-    private JsonNode uploadMultipleRequests(File file, UploadProgressListener listener, String videoId, int fileLength) throws IOException, ResponseException {
-        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
-        int              copiedBytes      = 0;
-        int              chunkCount       = (int) Math.ceil((double) fileLength / CHUNK_SIZE);
-        JsonNode         responseBody     = null;
-        for (int chunkNum = 0; chunkNum < chunkCount; chunkNum++) {
-
-            String chunkFileName = "upload-chunk-";
-            int    from          = copiedBytes;
-            copiedBytes = min(copiedBytes + CHUNK_SIZE, fileLength);
-            int chunkFileSize = copiedBytes - from;
-
-            String tmpdir = System.getProperty("java.io.tmpdir");
-
-            try (FileInputStream chunkStream = new FileInputStream(file)) {
-                //noinspection ResultOfMethodCallIgnored
-                chunkStream.skip(from);
-
-                byte[] b         = new byte[chunkFileSize];
-                File   chunkFile = File.createTempFile(tmpdir, chunkFileName);
-
-                RandomAccessFile randomAccessChunk = new RandomAccessFile(chunkFile, "rw");
-                randomAccessFile.readFully(b);
-                randomAccessChunk.write(b, 0, chunkFileSize);
-                final InputStream inputStream = new FileInputStream(chunkFile);
-                String            rangeHeader = "bytes " + from + "-" + (copiedBytes - 1) + "/" + fileLength;
-
-                RequestBuilder request = requestBuilderFactory
-                        .create(POST, "/videos/" + videoId + "/source")
-                        .withChunk(file.getName(), inputStream, chunkCount, chunkNum, listener)
-                        .withHeader("Content-Range", rangeHeader);
-
-                responseBody = requestExecutor.executeJson(request);
-
-                chunkFile.deleteOnExit();
-            }
-        }
-
-        return responseBody;
-    }
-
-    private JsonNode uploadSingleRequest(UploadProgressListener listener, File file, String videoId) throws ResponseException, IOException {
-        RequestBuilder request = requestBuilderFactory
-                .create(POST, "/videos/" + videoId + "/source")
-                .withFile(file, listener);
-
-        return requestExecutor.executeJson(request);
     }
 
     public Video uploadThumbnail(String videoId, File file) throws ResponseException, IOException {
@@ -209,15 +160,12 @@ public class UnirestVideoClient implements VideoClient {
         return deserializer.deserialize(responseBody.getObject());
     }
 
-
     public void delete(String videoId) throws ResponseException {
         RequestBuilder request = requestBuilderFactory
                 .create(DELETE, "/videos/" + videoId);
 
         requestExecutor.executeJson(request);
     }
-
-    /////////////////////////Iterators//////////////////////////////
 
     public Iterable<Video> list() throws ResponseException, IllegalArgumentException {
         return list(new QueryParams());
@@ -233,4 +181,54 @@ public class UnirestVideoClient implements VideoClient {
         ), new PageQuery()));
     }
 
+    /////////////////////////Iterators//////////////////////////////
+
+    private JsonNode uploadMultipleRequests(File file, UploadProgressListener listener, String videoId, int fileLength) throws IOException, ResponseException {
+        RandomAccessFile randomAccessFile = new RandomAccessFile(file, "r");
+        int copiedBytes = 0;
+        int chunkCount = (int) Math.ceil((double) fileLength / CHUNK_SIZE);
+        JsonNode responseBody = null;
+        for (int chunkNum = 0; chunkNum < chunkCount; chunkNum++) {
+
+            String chunkFileName = "upload-chunk-";
+            int from = copiedBytes;
+            copiedBytes = min(copiedBytes + CHUNK_SIZE, fileLength);
+            int chunkFileSize = copiedBytes - from;
+
+            String tmpdir = System.getProperty("java.io.tmpdir");
+
+            try (FileInputStream chunkStream = new FileInputStream(file)) {
+                //noinspection ResultOfMethodCallIgnored
+                chunkStream.skip(from);
+
+                byte[] b = new byte[chunkFileSize];
+                File chunkFile = File.createTempFile(tmpdir, chunkFileName);
+
+                RandomAccessFile randomAccessChunk = new RandomAccessFile(chunkFile, "rw");
+                randomAccessFile.readFully(b);
+                randomAccessChunk.write(b, 0, chunkFileSize);
+                final InputStream inputStream = new FileInputStream(chunkFile);
+                String rangeHeader = "bytes " + from + "-" + (copiedBytes - 1) + "/" + fileLength;
+
+                RequestBuilder request = requestBuilderFactory
+                        .create(POST, "/videos/" + videoId + "/source")
+                        .withChunk(file.getName(), inputStream, chunkCount, chunkNum, listener)
+                        .withHeader("Content-Range", rangeHeader);
+
+                responseBody = requestExecutor.executeJson(request);
+
+                chunkFile.deleteOnExit();
+            }
+        }
+
+        return responseBody;
+    }
+
+    private JsonNode uploadSingleRequest(UploadProgressListener listener, File file, String videoId) throws ResponseException, IOException {
+        RequestBuilder request = requestBuilderFactory
+                .create(POST, "/videos/" + videoId + "/source")
+                .withFile(file, listener);
+
+        return requestExecutor.executeJson(request);
+    }
 }
